a7ef43e5241fc10b26cf927b79a0739bd2c71005
sgoal <liuhdsgoal@gmail.com>
2018-10-22 12:27:42 +0800

    fixed the jre version

--- src/main/java/ant/ioc/xml/XMLBeanDefinitionReader.java
+++ src/main/java/ant/ioc/xml/XMLBeanDefinitionReader.java
@@ -1,15 +1,10 @@
 package ant.ioc.xml;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.Attributes.Name;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
